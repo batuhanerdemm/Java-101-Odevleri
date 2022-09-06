19d9fc51198416224e4518099bc0d93d0b448df0
Batuhan <erdemb215@gmail.com>
2022-09-07 01:53:00 +0300

    Odev 2
    Odev 2 eklendi

---
+++ java odev2/HelloApplication.java
@@ -0,0 +1,44 @@
+package com.example.nanana;
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+             // Değişkenleri olustur
+                int mat,fizik,tarih,türkçe,müzik;
+                //scanner sınıfımız tanımladık
+                Scanner inp= new Scanner(System.in);
+
+                //Kullanıcıdan değer al
+        System.out.println("Tutar Giriniz:");
+        mat=inp.nextInt();
+        double tutar= mat;
+        if (mat>1000){
+            System.out.println(tutar + tutar * 0.18);
+            System.out.println(tutar * 0.18);
+        }
+        else if (mat<1000){
+            System.out.println(tutar + tutar * 0.08);
+            System.out.println(tutar * 0.08);
+
+              }
+
+            }
+        }
+
+
