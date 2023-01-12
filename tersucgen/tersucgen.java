95449072fa756fe5c26bc836a6704241707d0039
Batuhan <erdemb215@gmail.com>
2023-01-12 21:55:43 +0300

    Update tersucgen.java

--- tersucgen/tersucgen.java
+++ tersucgen/tersucgen.java
@@ -2,21 +2,26 @@ package tekrar;
 import java.util.Scanner;
 public class tersucgen {
     public static void main(String[] args) {
-        int a,j,k;//Değişekenlerimizi tanımladık.
-        Scanner inp = new Scanner(System.in);
-        System.out.println("Basamak sayısını giriniz :");
-        a= inp.nextInt();
-        for (int i =0; i<a; a--) {//Döngümüzün ne kadar döneceğini belirtir.
-            for(j=i; j<10;j++) {//Üçgenimizi yaparken ekran çıktısının sol tarafındaki boşluğu belirtir.
-                System.out.print(" ");//Boşluğumuzu belirtiyoruz.
+             Scanner inp =new Scanner(System.in);
+
+                System.out.println("Bir Sayı Giriniz");
+                int n=inp.nextInt();
+
+                for(int i=0;i<n;i++){
+                    for(int j=0;j<=i;j++){
+                        System.out.print(" ");
+                    }
+                    for(int k=2*n-1;k>=(2*i+1);k--){
+                        System.out.print("*");
+
+                    }
+                    System.out.println();
+
+                }
+
+
             }
-            for (k=1; k<=a; k++) {//Yıldız sembolümüzün yazılmasını sağlar.
-                System.out.print("*");//Sembolümüzü belirledik ve çıktısını sağladık.
-                System.out.print(" ");//Yıldız sembolümüzün arasına boşluk koyulmasını sağlar.
-            }
-            System.out.println(" ");//Döngüden sonra diğer satıra atlamamızı sağlar.
-        }
 
         }
-    }
+
 
